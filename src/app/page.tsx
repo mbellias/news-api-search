797eb54eb3cc1c459207be2cf5f57fff385b671b
Michael Bellias <michaelbellias@mac-mini.mynetworksettings.com>
2024-09-03 12:50:33 -0400

    added next-themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import ArticleCards from '@/components/ArticleCards';
 import SearchForm from '@/components/SearchForm';
+import { ModeToggle } from '@/components/theme/ModeToggle';
 
 export default function Home() {
   return (
     <div className='w-full'>
+      <div className='p-2'>
+        <ModeToggle />
+      </div>
       <div className='flex flex-col justify-center items-center gap-4 m-4'>
-        <h1 className='text-2xl'>
-          Search 150,000 news sources worldwide using the <i>News API</i>
+        <h1 className='text-2xl '>
+          Search for news articles using the <i>News API</i>
         </h1>
         <SearchForm />
         <ArticleCards />
